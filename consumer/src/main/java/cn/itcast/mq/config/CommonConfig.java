80edb51623444f3a2dbe0cf0cb915cf63a933214
xgl <34029067@qq.com>
2022-08-24 17:33:59 +0800

    第五次提交，消息可靠性问题：确保发送的消息至少被消费一次--mq持久化--默认都是持久化  创建队列和交换机的时候，在消费者端创建，启动的时候自动加入容器 @Bean public DirectExchange simpleExchange(){ 三个参数：交换机名称、是否持久化、当没有queue与其绑定时是否自动删除 return new DirectExchange("simple.direct", true, false);} @Bean public Queue simpleQueue(){ 使用QueueBuilder构建队列，durable就是持久化的 return QueueBuilder.durable("simple.queue").build();}

--- consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
+++ consumer/src/main/java/cn/itcast/mq/config/CommonConfig.java
@@ -1,9 +1,6 @@
 package cn.itcast.mq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
-
+    //创建队列和交换机的时候，在消费者端创建，启动的时候自动加入容器
+    @Bean
+    public DirectExchange simpleExchange(){
+        // 三个参数：交换机名称、是否持久化、当没有queue与其绑定时是否自动删除
+        return new DirectExchange("simple.direct", true, false);
+    }
+    @Bean
+    public Queue simpleQueue(){
+        // 使用QueueBuilder构建队列，durable就是持久化的
+        return QueueBuilder.durable("simple.queue").build();
+    }
 }
